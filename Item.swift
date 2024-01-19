1d0b002072865604f43820b51416fc251b89b8a1
Ledockol <111918791+Ledockol@users.noreply.github.com>
2024-01-20 00:22:02 +0300

    Update Item.swift

--- Item.swift
+++ Item.swift
@@ -6,14 +6,14 @@
 //
 
 
-import Foundation
-import SwiftData
+// import Foundation
+// import SwiftData
 
-@Model
-final class Item {
-    var timestamp: Date
+// @Model
+// final class Item {
+//     var timestamp: Date
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
-}
+//     init(timestamp: Date) {
+//         self.timestamp = timestamp
+//     }
+// }
