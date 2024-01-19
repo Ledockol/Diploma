f990b4a03c682f6ec70372ea681e8ee459b7b6fa
Ledockol <111918791+Ledockol@users.noreply.github.com>
2024-01-20 00:21:24 +0300

    Update ShoeCleaningApp.swift

--- ShoeCleaningApp.swift
+++ ShoeCleaningApp.swift
@@ -4,28 +4,28 @@
 //
 //  Created by Андрей Беседин on 16.01.2024.
 //
-import SwiftUI
-import SwiftData
+// import SwiftUI
+// import SwiftData
 
 
-struct ShoeCleaningAppApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+// struct ShoeCleaningAppApp: App {
+//     var sharedModelContainer: ModelContainer = {
+//         let schema = Schema([
+//             Item.self,
+//         ])
+//         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+//         do {
+//             return try ModelContainer(for: schema, configurations: [modelConfiguration])
+//         } catch {
+//             fatalError("Could not create ModelContainer: \(error)")
+//         }
+//     }()
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
-    }
-}
+//     var body: some Scene {
+//         WindowGroup {
+//             ContentView()
+//         }
+//         .modelContainer(sharedModelContainer)
+//     }
+// }
